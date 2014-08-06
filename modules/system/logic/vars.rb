33e6bff4eff4fbef3fdb90a0d1cdec3d851b93c8
coolesting <coolesting@gmail.com>
2014-08-05 23:46:16 -0700

    update method _var for the function of adding a new variable if it hasn`t exsiting

--- modules/system/logic/vars.rb
+++ modules/system/logic/vars.rb
@@ -1,7 +1,20 @@
 helpers do
 
-	# return a string, others is nil
-	def _var key, tag = ''
+	# return a string, others is null value
+	#
+	# == Arguments
+	#
+	# key, this is key name
+	# tag, tag name
+	# val, default value, that will return if no this key in database
+	#
+	# == Example
+	#
+	# 	_var :home_page
+	# 	_var :home_page, 'www'
+	# 	_var :home_page, 'www', '/home/page'
+	#
+	def _var key, tag = '', val = ''
 		h 	= {:vkey => key.to_s}
 		ds 	= Sdb[:_vars].filter(h)
 
@@ -9,13 +22,20 @@ helpers do
 			tids = _tag_ids(:_vars, tag)
  			ds = ds.filter(:vid => tids)
 		end
- 		ds.empty? ? '' : ds.get(:vval)
+
+		# adding if the variable hasn't existing in database
+		if ds.empty?
+			_var_add({vkey: key, tag: tag, vval: val})
+			val
+		else
+			ds.get(:vval)
+		end
 	end
 
 	# return an array as value, split by ","
-	def _var2 key, tag = ''
-		val = _var key, tag
-		val.index(',') ? val.split(',') : (val == '' ? [] : [val])
+	def _var2 key, tag = '', val = '', sign = ","
+		val = _var key, tag, val
+		val.to_s.split(sign)
 	end
 
 	# update variable, create one if it doesn't exist
@@ -25,6 +45,8 @@ helpers do
 	end
 
 	def _var_add argv = {}
+		argv[:vkey] = argv[:vkey].to_s
+		argv[:vval] = argv[:vval].to_s
  		_submit(:_vars, :fkv => argv, :uniq => true) unless argv.empty?
 	end
 
